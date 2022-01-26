f4f3ca175b3943b2550a2e525a1ccc9e27797353
Jacob Cable <jacobcable94@gmail.com>
2022-01-26 08:40:41 +0000

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<style>
+		body {
+			background: #28171f;
+		}
+	</style>
+</svelte:head>
+
+<section class="text-zinc-200 text-center w-1/2 p-10 h-full items-center justify-center flex-col">
+	<h1 class="text-xl text-center">Jacob Cable</h1>
+	<p class="mt-4">Some content can go here</p>
+</section>
